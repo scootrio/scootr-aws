af057c584ec60c5ed3cbac97ffe8403805b555c7
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-11-27 11:13:43 -0800

    feat: add stubs/basic handler

--- test/index.js
+++ test/index.js
@@ -1,11 +1,24 @@
-const driver = require('../lib');
+const AWSDriver = require('../lib');
 const AWS = require('aws-sdk');
+const { compute, storage, project, event } = require('scootjs');
+const { http } = require('scootjs/events');
 
 AWS.config.getCredentials(function(err) {
   if (err) console.log(err.stack);
   // credentials not loaded
   else {
-    console.log("Access key:", AWS.config.credentials.accessKeyId);
-    console.log("Secret access key:", AWS.config.credentials.secretAccessKey);
+    console.log('Access key:', AWS.config.credentials.accessKeyId);
+    console.log('Secret access key:', AWS.config.credentials.secretAccessKey);
   }
 });
+
+let e = event('id').type(http('GET', 'users'));
+let c = compute('id');
+let s = storage('id');
+
+project('id')
+  .name('My Project')
+  .with(e)
+  .with(c)
+  .with(s)
+  .deploy(new AWSDriver());
