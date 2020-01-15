8fafc6b6e780af4050e5d6e5f39444b2a05af7a3
Braden Hitchcock <developer.hitchcock@gmail.com>
2020-01-15 13:53:58 -0800

    test: add tests for internal events

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,11 @@
-const { compute, storage, application, http, actions, types } = require('scootr');
+const { compute, storage, application, http, topic, actions, types } = require('scootr');
 const { driver, enums } = require('../lib/index');
 
 const code1 = `'use strict';
 
 const AWS = require('aws-sdk');
 
-module.exports = async event => {
+async function handler(event) {
   let client = new AWS.DynamoDB.DocumentClient();
   let params = {
     TableName: process.env.MyStorageConnection,
@@ -35,23 +35,66 @@ module.exports = async event => {
       )
     };
   }
-};`;
+}
+
+module.exports = handler;
+`;
 
 const code2 = `'use-strict';
 
-module.exports = async event => {
+const AWS = require('aws-sdk');
+
+function json(code, body) {
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Hello from my-other-compute.js!',
-        input: event
-      },
-      null,
-      2
-    )
+    statusCode: code,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }
+}
+
+async function handler(event) {
+
+  const params = {
+    Message: JSON.stringify({
+      message: 'Hello from MySecondCompute!'
+    }),
+    TopicArn: process.env.MyTopicConnection
   };
-};`;
+
+  const sns = new AWS.SNS();
+
+  try {
+    const result = await sns.publish(params).promise();
+    return json(200, {
+      message: 'Successfully sent message with ID ' + result.MessageId
+    });
+  } catch(err) {
+    return json(500, {
+      message: 'Failed to send SNS message',
+      details: err.message
+    })
+  }
+}
+
+module.exports = handler;
+`;
+
+const code3 = `'use strict';
+
+function handler(event){
+  const message = event.Records[0].Sns.Message;
+  const data = JSON.parse(message);
+  console.log('Got event from SNS topic with data', data);
+}
+
+module.exports = handler;
+`;
+
+const topic1 = topic('MyTopic')
+  .broker(enums.Brokers.SNS)
+  .name('my-topic-name');
 
 (async function() {
   const app = application('MyApplication')
@@ -80,13 +123,18 @@ module.exports = async event => {
       compute('MySecondCompute')
         .runtime(enums.Runtimes.Node12x)
         .code(code2)
-        .env('PORT', '4567')
-        .env('NAME', 'my-name')
         .on(
           http('MyPostEvent')
             .method('POST')
             .path('/event')
         )
+        .use(topic1, [actions.Create], 'MyTopicConnection')
+    )
+    .with(
+      compute('MyThirdCompute')
+        .runtime(enums.Runtimes.Node12x)
+        .code(code3)
+        .on(topic1)
     );
 
   try {
